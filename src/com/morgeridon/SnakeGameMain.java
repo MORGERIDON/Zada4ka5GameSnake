4cec2501084b60a5cda5cd36b6fa83de6104a10f
Morgeridon <Morgeridon@yandex.ru>
2022-07-17 11:52:58 +0500

    Добавил яблоки

--- src/com/morgeridon/SnakeGameMain.java
+++ src/com/morgeridon/SnakeGameMain.java
@@ -1,5 +1,6 @@
 package com.morgeridon;
 
+import com.morgeridon.objects.Apple;
 import com.morgeridon.objects.Snake;
 
 import javax.swing.*;
@@ -18,6 +19,7 @@ public class SnakeGameMain extends JPanel implements ActionListener {
 
 
     Snake snake = new Snake(5, 6, 5, 5);
+    Apple apple = new Apple();
     Timer timer = new Timer(1000 / speed, this);
 
     public SnakeGameMain() {
@@ -33,24 +35,29 @@ public class SnakeGameMain extends JPanel implements ActionListener {
         g.fillRect(0, 0, WIDTH * SCALE, HEIGHT * SCALE);
 
         for (int x = 0; x < WIDTH * SCALE; x += SCALE) {
-            g.setColor(Color.white);
+            g.setColor(Color.WHITE);
             g.drawLine(x, 0, x, HEIGHT * SCALE);
         }
 
         for (int y = 0; y < HEIGHT * SCALE; y += SCALE) {
-            g.setColor(Color.white);
+            g.setColor(Color.WHITE);
             g.drawLine(0, y, WIDTH * SCALE, y);
         }
 
-        for (int l = 0; l < snake.length; l++) {
+        g.setColor(Color.GREEN);
+        g.fillOval(apple.posX * SCALE + 3, apple.posY * SCALE + 3, SCALE - 3, SCALE - 3);
+
+        for (int l = 1; l < snake.length; l++) {
             g.setColor(Color.BLUE);
-            g.fillRect(snake.sX[l] * SCALE + 1, snake.sY[l] * SCALE + 1, SCALE - 2, SCALE - 2);
+            g.fillRect(snake.sX[l] * SCALE + 1, snake.sY[l] * SCALE + 1, SCALE - 1, SCALE - 1);
+            g.setColor(Color.CYAN);
+            g.fillRect(snake.sX[0] * SCALE + 1, snake.sY[0] * SCALE + 1, SCALE - 1, SCALE - 1);
         }
     }
 
     public static void main(String[] args) {
         jFrame = new JFrame("Rabo4a9 Zona");
-        jFrame.setSize(WIDTH * SCALE + 16, HEIGHT * SCALE + 6);
+        jFrame.setSize(WIDTH * SCALE + 16, HEIGHT * SCALE + 38);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.setResizable(false);
         jFrame.setLocationRelativeTo(null);
@@ -62,11 +69,37 @@ public class SnakeGameMain extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         snake.move();
+        if ((snake.sX[0] == apple.posX) && (snake.sY[0] == apple.posY)) {
+            apple.setRandomPosition();
+            snake.length++;
+        }
+        for (int l = 1; l < snake.length; l++) {
+            if ((snake.sX[l] == apple.posX) && (snake.sY[l] == apple.posY)) {
+                apple.setRandomPosition();
+            }
+            if ((snake.sX[l] == snake.sX[0]) && (snake.sY[l] == snake.sX[0])) {
+                timer.stop();
+                JOptionPane.showMessageDialog(null, "Game over, Replay?");
+//                int confirmDialog = JOptionPane.showConfirmDialog(null, "Game over, Replay?");
+//                System.out.println(confirmDialog);
+//                if (confirmDialog == 1) {
+                    jFrame.setVisible(false);
+                    snake.length = 2;
+                    apple.setRandomPosition();
+                    jFrame.setVisible(true);
+                    timer.start();
+//                }
+            }
+        }
+
+
+//        apple.setRandomPosition();
         repaint();
     }
+
     public class KeyBoard extends KeyAdapter {
-        public void keyPressed (KeyEvent event){
-            int key= event.getKeyCode();
+        public void keyPressed(KeyEvent event) {
+            int key = event.getKeyCode();
 
             if (key == KeyEvent.VK_UP && snake.direction != 2) snake.direction = 0;
             if (key == KeyEvent.VK_DOWN && snake.direction != 0) snake.direction = 2;
--- src/com/morgeridon/objects/Apple.java
+++ src/com/morgeridon/objects/Apple.java
@@ -1,4 +1,21 @@
 package com.morgeridon.objects;
 
+import com.morgeridon.SnakeGameMain;
+
 public class Apple {
+    public int posY;
+    public int posX;
+
+    public Apple(int posY, int posX) {
+        this.posY = posY;
+        this.posX = posX;
+    }
+    public void setRandomPosition(){
+        posX=Math.abs((int) (Math.random()* SnakeGameMain.WIDTH-1));
+        posY=Math.abs((int) (Math.random()* SnakeGameMain.HEIGHT-1));
+    }
+
+    public Apple(){
+        setRandomPosition();
+    }
 }
